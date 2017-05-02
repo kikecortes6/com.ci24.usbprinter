20d3b087797292bfe09d38ada2535df5f58f7b32
kikecortes6 <u1801864@unimilitar.edu.co>
2017-05-02 10:40:44 -0500

    Update usbprinter.js

--- www/usbprinter.js
+++ www/usbprinter.js
@@ -2,7 +2,15 @@ module.exports = (function() {
   var _print = function (args,successCallback, errorCallback) {
       cordova.exec(successCallback, errorCallback, "Printer", "printData", [args]);
     };
+  var _feedPaper = function (successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, "Printer", "feedPaper", []);
+  };
+  var _openPrinter = function (successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, "Printer", "openPrinter", []);
+  };
    return {
-    Print: _print
+    Print: _print,
+     FeedPaper:_feedPaper,
+     OpenPrinter:_openPrinter
     };
 })();
